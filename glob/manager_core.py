0daa8265439e3bc58b24705a1754349a6c6feb07
Dr.Lt.Data <dr.lt.data@gmail.com>
2025-07-04 17:54:26 +0900

    fixed: invalid default config.ini
    https://github.com/Comfy-Org/ComfyUI-Manager/issues/1967

--- glob/manager_core.py
+++ glob/manager_core.py
@@ -43,7 +43,7 @@ import manager_downloader
 from node_package import InstalledNodePackage
 
 
-version_code = [3, 33, 5]
+version_code = [3, 33, 6]
 version_str = f"V{version_code[0]}.{version_code[1]}" + (f'.{version_code[2]}' if len(version_code) > 2 else '')
 
 
@@ -1739,7 +1739,7 @@ def read_config():
 
     except Exception:
         import importlib.util
-        manager_util.use_uv = importlib.util.find_spec("uv")
+        manager_util.use_uv = importlib.util.find_spec("uv") is not None
         
         return {
             'http_channel_enabled': False,
